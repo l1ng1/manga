af83961146cdea1bc2765ce483bb52fb07d42904
Gulzhan <114820316+GulzhanKarakul@users.noreply.github.com>
2023-11-14 19:24:16 +0600

    Update PopularComp.js

--- manga/components/PopularComp.js
+++ manga/components/PopularComp.js
@@ -31,11 +31,11 @@ const PopularManga = (props) => {
         renderItem={({ item }) => (
           <View style={styles.box}>
             <Image resizeMode='contain' source={{ uri: "https://remanga.org" + item.img.low }} style={styles.coverImage} />
-            <Text style={styles.mangaName}>{item.main_name}</Text>
-            <Geners item={item} />
-          </View>
+                          <Text style={styles.mangaName}>{item.main_name}</Text>
+              <Geners item={item} />
+                      </View>
         )}
-        numColumns={5}
+numColumns={5}
         contentContainerStyle={styles.listContainer}
       />
     </View>
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        // backgroundColor: '#fff', // Фон контейнера
+// backgroundColor: '#fff', // Фон контейнера
     },
     listContainer: {
         width: '80%',
@@ -55,7 +55,7 @@ const styles = StyleSheet.create({
         width: '18%',
         margin: 10,
         borderWidth: 1,
-        borderColor: 'black',
+        borderColor: 'black', 
         padding: 20,
         borderRadius: 9,
         marginBottom: 15,
@@ -68,24 +68,24 @@ const styles = StyleSheet.create({
         height: 200,
     },
     mangaName: {
-        marginTop: 10,
+marginTop: 10,
         fontWeight: 'bold',
         fontSize: 18, // Уменьшим размер шрифта
         color: '#4682B4', // Цвет текста
         textAlign: 'center', // Выравнивание текста по центру
     },
     genresContainer: {
-        marginTop: 10,
+      marginTop: 10,
     },
     genresTitle: {
-        fontSize: 16,
-        fontWeight: 'bold',
+      fontSize: 16,
+      fontWeight: 'bold',
     },
     genresList: {
-        marginTop: 5,
+      marginTop: 5,
     },
     genreItem: {
-        fontSize: 14, // Уменьшим размер шрифта жанров
+      fontSize: 14, // Уменьшим размер шрифта жанров
     },
 });
 
